ae12e91d0b499851c80adbb547ab650445162d6b
Costa Michailidis <costa@innovationbound.com>
2022-10-17 13:41:25 -0400

    animated!

---
+++ scripts/animate-on-scroll.js
@@ -0,0 +1,25 @@
+!(function () {
+  "use strict"
+  window.addEventListener('load', function (event) {
+
+    // Set Up
+
+    var animated = document.querySelectorAll('.animate')
+
+    animated.forEach(function (element) {
+      element.classList.add('hidden')
+    })
+
+    // Animate On Scroll
+
+    var observer = new IntersectionObserver( function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove('hidden')
+        }
+      })
+    })
+    animated.forEach((element) => observer.observe(element))
+
+  })
+}());
